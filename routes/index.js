4f45b4f1de41159df8279e892c625876bb631a0d
mi1682516 <mi1682516@gmail.com>
2022-01-20 06:43:01 +0700

    menambahkan fungsionalitas database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+var Session = require('../utils/session')
+var Driver = require('../utils/driver')
+var driver = Driver(process.env);
 var express = require('express');
+var session = Session(driver);
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('/index/')
+  res.send(session)
 });
 
 module.exports = router;
