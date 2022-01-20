4bf7dce521524f6668e59475f8a6e0c5c517f173
mi1682516 <mi1682516@gmail.com>
2022-01-21 04:55:22 +0700

    update quury login

--- routes/login.js
+++ routes/login.js
@@ -6,10 +6,24 @@ var express = require('express');
 var session = Session(driver);
 var router = express.Router();
 
-router.post('/', (req, res, next) => {
+router.post('/',(req, res, next) => {
   passport.authenticate('local',(e,u) => {
     res.send({e,u})
   })(req, res, next)
+})
+
+router.post('/submit',async(req,res) => {
+  var _session = session.create(
+   session.driver
+  )
+  var test = await _session.run(
+    `match(user)<-[rl:profile]
+    -(p) where user.username=$
+    username and user.password
+    =$password return user,p`,
+    Object( { ...req.body } )
+  )
+  res.send(test.records)
 })
 
 module.exports = router
